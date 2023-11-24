d5bfb27507fe314a8f2f9eb3f33c56cef5fc7e9a
02Alexis <m_chael1012@hotmail.com>
2023-11-24 00:45:02 -0500

    changing design to navigation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,6 +9,56 @@ import Logo from "../../assets/logo.svg";
 import ModalNavbar from "../ModalNavbar/ModalNavbar";
 import SocialMediaButton from "../SocialMediaButton/SocialMediaButton";
 
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import { CiSearch } from "react-icons/ci";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  // paddingLeft: "0.5rem",
+
+  paddingRight: "5rem",
+  width: "350%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "white",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
 const Header = () => {
   const [show, setShow] = useState(false);
   const menu = useRef();
@@ -41,6 +91,15 @@ const Header = () => {
           <motion.div whileHover={{ y: -2 }}>
             <img src={Logo} className="custom-logo-icon" />
           </motion.div>
+          <Search>
+            <SearchIconWrapper>
+              <CiSearch />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
         </Link>
         <nav className="custom-nav">
           <button className="custom-menu-button" onClick={() => setShow(!show)}>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,43 +1,116 @@
 import "./Navbar.scss";
+
+import * as React from "react";
 import { Link } from "react-router-dom";
+import {
+  LuLuggage,
+  LuUsers,
+  LuFileText,
+  LuMessageSquare,
+} from "react-icons/lu";
+import { MdNotificationsNone } from "react-icons/md";
+
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import MenuItem from "@mui/material/MenuItem";
+import Tooltip from "@mui/material/Tooltip";
+import Avatar from "@mui/material/Avatar";
+import Badge from "@mui/material/Badge";
+import Menu from "@mui/material/Menu";
+import Box from "@mui/material/Box";
 
 const links = [
   {
     route: "/",
-    label: "RedEAmérica",
+    icon: <LuLuggage />,
+    label: "Jobs",
   },
   {
-    route: "#conocimiento",
-    label: "Conocimiento Colaborativo",
+    route: "#connect",
+    icon: <LuUsers />,
+    label: "Conection",
   },
   {
-    route: "#dialogo",
-    label: "Díalogo Regional",
-  },
-  {
-    route: "#noticias",
-    label: "Noticias",
-  },
-  {
-    route: "#contact",
-    label: "Contacto",
+    route: "#pages",
+    icon: <LuFileText />,
+    label: "Pages",
   },
 ];
 
+const settings = ["My Account", "Edit Profile", "Logout", "Logout"];
+
 const Navbar = ({ handleMouseEnter, handleMouseLeave, onClose }) => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <ul className="navbar">
-      {links.map(({ route, label }) => (
+      {links.map(({ route, icon, label }) => (
         <li
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
           key={label}
         >
           <Link href={route} onClick={onClose} smooth={true}>
+            <span>{icon}</span>
+
             {label}
           </Link>
         </li>
       ))}
+      <div className="navbar-separator" />
+
+      <div className="navbar-icons">
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <LuMessageSquare />
+          </Badge>
+        </IconButton>
+        <IconButton aria-label="show 17 new notifications" color="inherit">
+          <Badge badgeContent={17} color="primary">
+            <MdNotificationsNone />
+          </Badge>
+        </IconButton>
+      </div>
+
+      {/* Separación entre iconos y configuración de usuario */}
+      <div className="navbar-separator" />
+
+      <Box sx={{ flexGrow: 0 }}>
+        <Tooltip title="Open settings">
+          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          </IconButton>
+        </Tooltip>
+        <Menu
+          sx={{ mt: "45px" }}
+          id="menu-appbar"
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {settings.map((setting) => (
+            <MenuItem key={setting} onClick={handleCloseUserMenu}>
+              <Typography textAlign="center">{setting}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
     </ul>
   );
 };
