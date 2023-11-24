79a5a8210cf6a784d65daca718a1b134385f1e69
Yesid Vanegas <yesidvanegas68@gmail.com>
2023-11-23 22:51:52 -0500

    Creacion de la card de personas destacadas

--- src/Components/Friends/Friends.jsx
+++ src/Components/Friends/Friends.jsx
@@ -1,7 +1,82 @@
+import './StylesCardsFriends.scss'
 
+const users = [
+  {
+    nombre: "Juan",
+    usuario: "@juanito",
+    imagen: "https://placekitten.com/200/300",
+  },
+  {
+    nombre: "María",
+    usuario: "@maria22",
+    imagen: "https://placebeard.it/200x300",
+  },
+  {
+    nombre: "Carlos",
+    usuario: "@carlitos",
+    imagen: "https://placecorgi.com/200/300",
+  },
+  {
+    nombre: "Ana",
+    usuario: "@anita",
+    imagen: "https://placekitten.com/201/301",
+  },
+  {
+    nombre: "Luis",
+    usuario: "@luisito",
+    imagen: "https://placebeard.it/201x301",
+  },
+  {
+    nombre: "Laura",
+    usuario: "@laurita",
+    imagen: "https://placecorgi.com/201/301",
+  },
+  {
+    nombre: "Pedro",
+    usuario: "@pedrito",
+    imagen: "https://placekitten.com/202/302",
+  },
+  {
+    nombre: "Isabel",
+    usuario: "@isabelita",
+    imagen: "https://placebeard.it/202x302",
+  },
+  {
+    nombre: "Miguel",
+    usuario: "@miguelito",
+    imagen: "https://placecorgi.com/203/300",
+  },
+  {
+    nombre: "Sofía",
+    usuario: "@sofi86",
+    imagen: "https://placekitten.com/203/300",
+  },
+]
+
+console.log(users)
 const Friends = () => {
   return (
-    <div>Friends</div>
+    <div className='cardFriends'>
+      <div className='destacadas'>
+        <p>Personas Destacadas</p>
+      </div>
+      {
+        users.map((user, index) => (
+          <div className="cardFriends__info" key={index}>
+            <div className='usuario'>
+              <img src={user.imagen} alt="" />
+              <div className='infoUsuario'>
+                <span className='name'>{user.nombre}</span>
+                <span className='nameUsuario'>{user.usuario}</span>
+              </div>
+            </div>
+            <button>
+              Connent
+            </button>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
--- src/Components/Perfil/Perfil.jsx
+++ src/Components/Perfil/Perfil.jsx
@@ -1,10 +1,13 @@
 import CardPerfil from "../CardPerfil/CardPerfil"
+import Friends from "../Friends/Friends"
+import './StylesPerfil.scss'
 
 const Perfil = () => {
   return (
-    <>
+    <div className="container">
       <CardPerfil/>
-    </>
+      <Friends/>
+    </div>
   )
 }
 
